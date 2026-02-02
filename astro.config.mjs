975402a0e6d8ba528657bbc4bf29fdba525c1f40
Florian (Feuermagier) <floseligmann@web.de>
2026-02-02 10:42:04 +0100

    ci configs for builds and deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://alr-kit.de",
   compressHTML: true,
   build: {
     inlineStylesheets: 'always',
